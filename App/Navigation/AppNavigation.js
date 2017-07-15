aa367c41e41c114331f43bf94bd68343b41f00d0
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2017-07-16 02:13:43 +0500

    Added explore, favorite, search and settings screens

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,22 @@
-import { StackNavigator } from 'react-navigation'
-import LaunchScreen from '../Containers/LaunchScreen'
+import { TabNavigator } from 'react-navigation'
+import SearchScreen from '../Containers/SearchScreen'
+import ExploreScreen from '../Containers/ExploreScreen'
+import FavoritesScreen from '../Containers/FavoritesScreen'
+import SettingsScreen from '../Containers/SettingsScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
-const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+const PrimaryNav = TabNavigator({
+  search: { screen: SearchScreen },
+  explore: { screen: ExploreScreen },
+  favorites: { screen: FavoritesScreen },
+  settings: { screen: SettingsScreen }
+
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'explore',
   navigationOptions: {
     headerStyle: styles.header
   }
