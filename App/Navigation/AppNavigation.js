04109cf7d4fd7b9430b0f76dd15d0510383b1861
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2017-07-22 11:39:42 +0500

    Navigating to POI detail screen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -4,7 +4,6 @@ import ExploreScreen from "../Containers/ExploreScreen";
 import FavoritesScreen from "../Containers/FavoritesScreen";
 import SettingsScreen from "../Containers/SettingsScreen";
 import PoiDetailScreen from "../Containers/PoiDetailScreen";
-import {Colors} from "../Themes";
 
 import styles from "./Styles/NavigationStyles";
 
@@ -12,42 +11,33 @@ import styles from "./Styles/NavigationStyles";
 
 const MainNav = TabNavigator({
   search: {
-    // path: 'search',
     screen: SearchScreen
   },
   explore: {
-    // path: 'explore',
     screen: ExploreScreen
   },
   favorites: {
-    // path: 'favorites',
     screen: FavoritesScreen
   },
   settings: {
-    // path: 'settings',
     screen: SettingsScreen
   },
 }, {
   headerMode: 'none',
   initialRouteName: 'explore',
   navigationOptions: {
-    headerStyle: styles.header
+    headerStyle: styles.navBar
   }
 })
 
 const PrimaryNav = StackNavigator({
     detail: {
-      // path: 'detail',
       screen: PoiDetailScreen,
       navigationOptions: {
         headerStyle: styles.navBar,
-        headerTitleStyle: styles.navBarTitle,
-        headerBackTitleStyle: styles.backTitleStyle,
-        headerTintColor: Colors.snow
       }
     },
     main: {
-      // path: 'main',
       screen: MainNav
     }
   }, {
