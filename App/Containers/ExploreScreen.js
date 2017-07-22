3fa2f9a12b9e9b21236879665cbfba9cbb655c34
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2017-07-22 22:00:01 +0500

    Updated icon showing logic

--- App/Containers/ExploreScreen.js
+++ App/Containers/ExploreScreen.js
@@ -84,17 +84,27 @@ class ExploreScreen extends Component {
         let message = ''
         let icon = null
         const types = _.intersection(point.types, [2, 4])
+
+        // if contains only type 2 in array then "Boat Ramp"
+        // if contains only type 4 in array then "Marina"
+        // if contains both type 2 and type 4 then "Boat Ramp / Marina"
+
         if (types.length === 2) {
           message = 'Boat Ramp / Marina'
-          icon = Images.boatRamp
         }
         else if (_.indexOf(types, 2) !== -1) {
           message = 'Boat Ramp'
-          icon = Images.boatRamp
-
         }
         else if (_.indexOf(types, 4) !== -1) {
           message = 'Marina'
+        }
+
+        // if it contains a 2 it would get the "Ramp" icon
+        // if it contains a 4 and not a 2 it would get the "Anchor" icon
+        if (_.indexOf(types, 2) !== -1) {
+          icon = Images.boatRamp
+        }
+        else if (_.indexOf(types, 4) !== -1) {
           icon = Images.anchor
         }
 
